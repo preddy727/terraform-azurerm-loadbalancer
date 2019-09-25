9629b09d038ac9f8e89edec664b3fbfccbf91ae9
preddy727 <47993564+preddy727@users.noreply.github.com>
2019-09-24 23:32:48 -0500

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -15,7 +15,7 @@ output "azurerm_lb_id" {
 
 output "azurerm_lb_frontend_ip_configuration" {
   description = "the frontend_ip_configuration for the azurerm_lb resource"
-  value       = "${azurerm_lb.azlb.frontend_ip_configuration}"
+  value       = "${azurerm_lb.azlb.frontend_ip_configuration.*.id}"
 }
 
 output "azurerm_lb_probe_ids" {
@@ -28,6 +28,7 @@ output "azurerm_lb_nat_rule_ids" {
   value       = "${azurerm_lb_nat_rule.azlb.*.id}"
 }
 
+/*
 output "azurerm_public_ip_id" {
   description = "the id for the azurerm_lb_public_ip resource"
   value       = "${azurerm_public_ip.azlb.*.id}"
@@ -37,7 +38,7 @@ output "azurerm_public_ip_address" {
   description = "the ip address for the azurerm_lb_public_ip resource"
   value       = "${azurerm_public_ip.azlb.*.ip_address}"
 }
-
+*/
 output "azurerm_lb_backend_address_pool_id" {
   description = "the id for the azurerm_lb_backend_address_pool resource"
   value       = "${azurerm_lb_backend_address_pool.azlb.id}"
